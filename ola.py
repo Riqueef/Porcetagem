46c4061286e03ec4be77da9dce4689d4cf80276f
Riqueef <freitashenrique827@gmail.com>
2025-08-13 13:10:00 -0300

    Add files via upload

---
+++ ola.py
@@ -0,0 +1,8 @@
+
+valor_total = float(input("digite o valor total:"))
+porcentagem = float(input("digita a porcetagem:"))
+
+valor_porcetagem = valor_total * (porcentagem / 100)
+
+print (f"o valor de: {porcentagem} % de {valor_total} é: {valor_porcetagem: .1f}")
+
